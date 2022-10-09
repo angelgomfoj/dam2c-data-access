7212371ff6fdb31b7093ed9fbd5d8c54ae7e2efc
angelgomfoj <angel.gomfoj@educa.jcyl.es>
2022-10-09 04:14:32 +0200

    Exercise 3 from UT2 modification

--- exercises/src/main/java/com/venancio/blanco/exercises2/Add2022_03.java
+++ exercises/src/main/java/com/venancio/blanco/exercises2/Add2022_03.java
@@ -1,42 +1,30 @@
 package com.venancio.blanco.exercises2;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Add2022_03 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Introduce la ruta de un archivo existente:");
 		File archive = new File(sc.nextLine());
 		sc.close();
 		if (archive.isFile() && archive.length() > 0) {
 			System.out.println("Introduce un formato de salida: caracter, enteros, hexadecimal, octal o binario,");
-			String format = sc.nextLine();
+			String format = sc.nextLine().toLowerCase();
 
 			switch (format) {
 			case "caracter":
-				readFile();
-				writeCharFile();
-				break;
 			case "enteros":
-				readFile();
-				writeIntFile();
-
-				break;
 			case "hexadecimal":
-				readFile();
-				writeHexaFile();
-
-				break;
 			case "octal":
-				readFile();
-				writeOctalFile();
-				break;
 			case "binario":
-				readFile();
-				writeBinaryFile();
+				createFormatFile(archive, format);
 				break;
-
 			default:
 				System.err.println("El formato introducido no es valido.\nTerminando programa...");
 				break;
@@ -47,33 +35,25 @@ public class Add2022_03 {
 		}
 	}
 
-	private static void writeBinaryFile() {
-		// TODO Auto-generated method stub
+	private static void createFormatFile(File archive, String format) throws IOException {
+		String fileName = archive.getName();
+		if (fileName.contains(".")) {
+			int temp = fileName.lastIndexOf('.');
+			fileName = fileName.substring(0, temp);
+		}
+		String newFileName = fileName + "_" + format + ".txt";
+
+		File newArchive = new File(archive.getParent() + File.separatorChar + newFileName);
+		newArchive.createNewFile();
+
+		// 2nd part
+		try (FileInputStream is = new FileInputStream(archive);
+				FileOutputStream os = new FileOutputStream(newArchive);) {
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 
 	}
 
-	private static void writeOctalFile() {
-		// TODO Auto-generated method stub
-
-	}
-
-	private static void writeHexaFile() {
-		// TODO Auto-generated method stub
-
-	}
-
-	private static void writeIntFile() {
-		// TODO Auto-generated method stub
-
-	}
-
-	private static void writeCharFile() {
-		// TODO Auto-generated method stub
-
-	}
-
-	private static void readFile() {
-		// TODO Auto-generated method stub
-
-	}
 }
