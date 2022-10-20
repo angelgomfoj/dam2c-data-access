89245ff671127669b9327d096fce9d78ccfc6338
angelgomfoj <angel.gomfoj@educa.jcyl.es>
2022-10-20 20:11:26 +0200

    Exercise 8 done

--- exercises/src/main/java/com/venancio/blanco/exercises2/Add2022_08.java
+++ exercises/src/main/java/com/venancio/blanco/exercises2/Add2022_08.java
@@ -1,5 +1,32 @@
 package com.venancio.blanco.exercises2;
 
-public class Add2022_08 {
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
+public class Add2022_08 {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		File archive;
+		String line = "";
+		do {
+			System.out.println("Introduce una ruta que no exista.");
+			archive = new File(sc.nextLine());
+		} while (archive.exists());
+		archive.createNewFile();
+		System.out
+				.println("Introduce tantas lineas de texto como desees.\nEscribe: Salir... para terminar el programa");
+		try (FileWriter fw = new FileWriter(archive)) {
+			while (!line.equals("Salir...")) {
+				line = sc.nextLine();
+				if (!line.equals("Salir...")) {
+					fw.write(line + "\n");
+				}
+			}
+			sc.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
