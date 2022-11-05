8332b5fef1454fb83905f7ee0957f67f6c7e1ea3
angelgomfoj <angel.gomfoj@educa.jcyl.es>
2022-11-05 18:46:08 +0100

    Exercise Reg.db almost finished

--- exercises/src/main/java/ejercicioangelgomezfoj/GestionaRegStor.java
+++ exercises/src/main/java/ejercicioangelgomezfoj/GestionaRegStor.java
@@ -1,9 +1,12 @@
 package ejercicioangelgomezfoj;
 
+import java.io.EOFException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -17,9 +20,10 @@ public class GestionaRegStor {
 		} while (!archive.isFile() || archive.length() == 0);
 
 		System.out.println(
-				"Introduce el numero correspondiente a la opcion deseada:\n[1] Nuevo registro\n[2] Eliminar registro\n[3] Modificar registro\n[4] Leer registro\n[5] Buscar registros");
+				"Introduce el numero correspondiente a la opcion deseada:\n[1] Nuevo registro\n[2] Eliminar registro\n[3] Modificar registro\n[4] Leer registro\n[5] Buscar registros\n[6] Mostrar registros\n[7] Compactar archivo");
 
 		int option = Integer.valueOf(sc.nextLine());
+		List<Register> registers = new ArrayList<>();
 
 		switch (option) {
 		case 1:
@@ -109,6 +113,168 @@ public class GestionaRegStor {
 			}
 			break;
 		case 5:
+			try (RandomAccessFile raf = new RandomAccessFile(archive, "rw")) {
+				Register register;
+
+				while (true) {
+					register = new Register();
+					byte[] byteName = new byte[32];
+					register.setId(raf.readInt());
+					raf.read(byteName);
+					register.setName(new String(byteName));
+					register.setDni(raf.readInt());
+					register.setSalary(raf.readDouble());
+					register.setGender(raf.readBoolean());
+					register.setWorkRemote(raf.readBoolean());
+					registers.add(register);
+				}
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (EOFException e) {
+				System.out.println(
+						"Selecciona un campo para el filtrado:\n[1] Id  [2] Nombre  [3] Dni  [4] Salario  [5] Sexo  [6] Lugar de trabajo");
+				int fieldNum = Integer.valueOf(sc.nextLine());
+				int i;
+				switch (fieldNum) {
+				case 1:
+					System.out.println("Introduce el id:");
+					int searchId = Integer.valueOf(sc.nextLine());
+					i = 0;
+					for (Register register : registers) {
+						if (register.getId() == searchId) {
+							System.out.println(register.toString());
+							i++;
+						}
+					}
+					if (i == 0) {
+						System.out.println("No se ha encontrado ninguna coincidencia");
+					}
+					break;
+				case 2:
+					System.out.println("Introduce el nombre:");
+					String searchName = sc.nextLine();
+					i = 0;
+					for (Register register : registers) {
+						if (register.getName().equalsIgnoreCase(searchName) && register.getId() != 0) {
+							System.out.println(register.toString());
+							i++;
+						}
+					}
+					if (i == 0) {
+						System.out.println("No se ha encontrado ninguna coincidencia");
+					}
+					break;
+				case 3:
+					System.out.println("Introduce el DNI:");
+					int searchDni = Integer.valueOf(sc.nextLine());
+					i = 0;
+					for (Register register : registers) {
+						if (register.getDni() == searchDni && register.getId() != 0) {
+							System.out.println(register.toString());
+							i++;
+						}
+					}
+					if (i == 0) {
+						System.out.println("No se ha encontrado ninguna coincidencia");
+					}
+					break;
+				case 4:
+					System.out.println("Introduce el Salario:");
+					double searchSalary = Double.valueOf(sc.nextLine());
+					i = 0;
+					for (Register register : registers) {
+						if (register.getSalary() == searchSalary && register.getId() != 0) {
+							System.out.println(register.toString());
+							i++;
+						}
+					}
+					if (i == 0) {
+						System.out.println("No se ha encontrado ninguna coincidencia");
+					}
+					break;
+				case 5:
+					System.out.println("Introduce el sexo: [Hh/Mm] (Hombre/Mujer)");
+					String searchGender = Character.toString(sc.nextLine().charAt(0));
+					boolean gender;
+					if (searchGender.equalsIgnoreCase("h")) {
+						gender = true;
+					} else if (searchGender.equalsIgnoreCase("m")) {
+						gender = false;
+					} else {
+						System.err.println("[ERROR] El valor introducido no es valido.");
+						break;
+					}
+					i = 0;
+					for (Register register : registers) {
+						if (register.isGender() == gender && register.getId() != 0) {
+							System.out.println(register.toString());
+							i++;
+						}
+					}
+					if (i == 0) {
+						System.out.println("No se ha encontrado ninguna coincidencia");
+					}
+					break;
+				case 6:
+					System.out.println("Introduce el lugar de trabajo: [Cc/Oo] (Casa/Oficina)");
+					String searchWorkPlace = Character.toString(sc.nextLine().charAt(0));
+					boolean workPlace;
+					if (searchWorkPlace.equalsIgnoreCase("c")) {
+						workPlace = true;
+					} else if (searchWorkPlace.equalsIgnoreCase("o")) {
+						workPlace = false;
+					} else {
+						System.err.println("[ERROR] El valor introducido no es valido.");
+						break;
+					}
+					i = 0;
+					for (Register register : registers) {
+						if (register.isWorkRemote() == workPlace && register.getId() != 0) {
+							System.out.println(register.toString());
+							i++;
+						}
+					}
+					if (i == 0) {
+						System.out.println("No se ha encontrado ninguna coincidencia");
+					}
+					break;
+				default:
+					System.err.println("Opcion invalida.");
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			break;
+		case 6:
+			try (RandomAccessFile raf = new RandomAccessFile(archive, "r")) {
+				raf.seek(0);
+				Register register;
+				while (true) {
+					register = new Register();
+					byte[] byteName = new byte[32];
+					register.setId(raf.readInt());
+					raf.read(byteName);
+					register.setName(new String(byteName));
+					register.setDni(raf.readInt());
+					register.setSalary(raf.readDouble());
+					register.setGender(raf.readBoolean());
+					register.setWorkRemote(raf.readBoolean());
+					registers.add(register);
+				}
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (EOFException e) {
+				for (Register register : registers) {
+					if (register.getId() != 0) {
+						System.out.println(register.toString());
+					}
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			break;
+		case 7:
+			// compactar archivo
 			break;
 		default:
 			System.err.println("[ERROR] Opcion no valida.");
